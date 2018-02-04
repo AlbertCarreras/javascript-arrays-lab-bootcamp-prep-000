a40a8691354a09274dddcffa4f7c6311c0f0ba62
Alberto Carreras <acarrerasc@gmail.com>
2018-02-04 16:39:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function destructivelyAppendKitten(name) {
   kittens.push(name)
   return kittens
 }
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
