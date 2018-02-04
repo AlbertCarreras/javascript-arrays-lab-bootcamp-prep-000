206a4aadc8540d6d638b11269937c3ab02c9c2d6
Alberto Carreras <acarrerasc@gmail.com>
2018-02-04 16:41:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+var array = ["Milo", "Otis", "Garfield"]
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
   kittens.push(name)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
   return kittens
 }
 
